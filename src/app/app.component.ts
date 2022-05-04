36417506c2f449ba041654deb6ea96f8c519b9f7
Ladislav Šalom <salom@devhand.com>
2022-05-04 15:05:52 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+declare global {
+    interface Window { productFruits: any; }
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-sample';
+    title = 'angular-sample';
+
+    loadPf = () => {
+
+        const userInformation = { username: 'test-username' }
+
+        // @ts-ignore
+        window.productFruitsUser = userInformation;
+
+        (function (w: PfWindow, d, u, c, l) {
+            w.productFruits = w.productFruits || {};
+            w.productFruits.language = l; w.productFruits.code = c;
+            var a = d.getElementsByTagName('head')[0];
+            var r = d.createElement('script'); r.async = true;
+            r.src = u + '?c=' + c;
+            a.appendChild(r);
+        })(window as PfWindow, document, 'https://my.ohio.pf.dev/static/script.js', 'XbWsMpmZpxLmV24q', 'en'); // Replace 'en' with a proper language code
+    }
+}
+
+type PfWindow = {
+    productFruits: any;
 }
