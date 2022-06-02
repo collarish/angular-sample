81620c24db1bcdc3f33aad30220ca73c2c03680c
Ladislav Šalom <salom@devhand.com>
2022-06-02 22:36:40 +0200

    update to new script version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { productFruits } from 'product-fruits';
 
 declare global {
     interface Window { productFruits: any; }
@@ -12,21 +13,29 @@ declare global {
 export class AppComponent {
     title = 'angular-sample';
 
+    // loadPfLegacy = () => {
+
+    //     const userInformation = { username: 'test-username' }
+
+    //     // @ts-ignore
+    //     window.productFruitsUser = userInformation;
+
+    //     (function (w: PfWindow, d, u, c, l) {
+    //         w.productFruits = w.productFruits || {};
+    //         w.productFruits.language = l; w.productFruits.code = c;
+    //         var a = d.getElementsByTagName('head')[0];
+    //         var r = d.createElement('script'); r.async = true;
+    //         r.src = u + '?c=' + c;
+    //         a.appendChild(r);
+    //     })(window as PfWindow, document, 'https://app.productfruits.com/static/script.js', 'YOUR CODE', 'en'); // Replace 'en' with a proper language code
+    // }
+
     loadPf = () => {
-
-        const userInformation = { username: 'test-username' }
-
-        // @ts-ignore
-        window.productFruitsUser = userInformation;
-
-        (function (w: PfWindow, d, u, c, l) {
-            w.productFruits = w.productFruits || {};
-            w.productFruits.language = l; w.productFruits.code = c;
-            var a = d.getElementsByTagName('head')[0];
-            var r = d.createElement('script'); r.async = true;
-            r.src = u + '?c=' + c;
-            a.appendChild(r);
-        })(window as PfWindow, document, 'https://app.productfruits.com/static/script.js', 'YOUR CODE', 'en'); // Replace 'en' with a proper language code
+        productFruits.init('n1AYOgN22v6hx0ua', 'en', { username: 'npm.test' }, { disableLocationChangeDetection: true });
+        
+        productFruits.safeExec($pf => {
+            console.log($pf);
+        })
     }
 }
 
